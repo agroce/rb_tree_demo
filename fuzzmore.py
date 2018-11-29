607497714a9d95bbe1b2140285083f384cbdb634
Alex Groce <agroce@gmail.com>
2018-11-29 06:02:42 -0700

    refactor

--- fuzzmore.py
+++ fuzzmore.py
@@ -33,46 +33,7 @@ for line in open("fuzz.out"):
 if len(coverages) > 0:
     print("COVERAGE CHANGE WITH NEW FUZZING:", coverages[0], "TO", coverages[-1])
 print("THERE ARE NOW", len(glob.glob("newcorpus/*")), "TESTS")
-corpus = "newcorpus"
 
-for f in glob.glob(corpus + "/*"):
-    with open("test.out", 'w') as outf:
-        subprocess.call([cmd + f], shell=True, stdout=outf, stderr=outf)
-    crashline = None
-    fatalline = None
-    stepcount = 0
-    with open("test.out", 'r') as inf:
-        for line in inf:
-            if "Crash" in line:
-                crashline = line
-            if "UndefinedBehaviorSanitizer" in line:
-                crashline = line
-            if "AddressSanitizer" in line:
-                crashline = line                
-            if "FATAL" in line:
-                fatalline = line
-            if "STEP" in line:
-                stepcount += 1
-    if crashline is not None:
-        if crashline not in crashes:
-            crashes[crashline] = (stepcount, f)
-        else:
-            if stepcount < crashes[crashline][0]:
-                crashes[crashline] = (stepcount, f)
-    if fatalline is not None:
-        if fatalline not in fatals:
-            fatals[fatalline] = (stepcount, f)
-        else:
-            if stepcount < fatals[fatalline][0]:
-                fatals[fatalline] = (stepcount, f)
-for fatal in fatals:
-    print (fatal, fatals[fatal])
-for crash in crashes:
-    print (crash, crashes[crash])
+r = subprocess.call(["python findfailures.py newcorpus"], shell=True)
+sys.exit(r)
 
-print (len(fatals), "FATALS", len(crashes), "CRASHES")
-if len(fatals) > 0:
-    sys.exit(255)
-if len(crashes) > 0:
-    sys.exit(255)
-sys.exit(0)
