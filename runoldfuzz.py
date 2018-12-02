adbdf2b3b0f409d10de03f2352cf4aebe46fb499
Alex Groce <agroce@gmail.com>
2018-12-02 15:52:39 -0700

    fix indentation

--- runoldfuzz.py
+++ runoldfuzz.py
@@ -3,9 +3,11 @@ import subprocess
 import sys
 
 subprocess.call(["make fuzz_rb"], shell=True)
+
 with open("fuzz.result",'w') as soutf:
     with open("fuzz.error",'w') as serrf:    
-    subprocess.call(["./fuzz_rb"], shell=True, stdout=soutf, stderr=serrf)
+        subprocess.call(["./fuzz_rb"], shell=True, stdout=soutf, stderr=serrf)
+
 with open("fuzz.result",'r') as f:
     for l in f:
         print(l,end="")
