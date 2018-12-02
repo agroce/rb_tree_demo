0302674a1bdd1e67a9181da51d867e2af4149cfa
Alex Groce <agroce@gmail.com>
2018-12-02 15:43:43 -0700

    fix output up

--- runoldfuzz.py
+++ runoldfuzz.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import subprocess
 import sys
 
@@ -5,6 +6,7 @@ subprocess.call(["make fuzz_rb"], shell=True)
 subprocess.call(["./fuzz_rb >& fuzz.result"], shell=True)
 with open("fuzz.result",'r') as f:
     for l in f:
+        print(l,end="")
         if "Done fuzzing" in l:
             sys.exit(0)
 sys.exit(255)
