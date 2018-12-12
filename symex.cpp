94d70da867b0629220857883455c2da8b35913fe
Alex Groce <agroce@gmail.com>
2018-12-12 10:59:17 -0700

    more checks, longer test

--- symex.cpp
+++ symex.cpp
@@ -39,6 +39,27 @@ void *voidP() {
   return (void *)p;
 }
 
+int idx;
+
+void InorderTreeVerify(rb_red_blk_tree* tree, rb_red_blk_node* x) {
+  if (x != tree->nil) {
+    struct elt_t e;
+    InorderTreeVerify(tree,x->left);
+    e = containerGet (idx);
+    ASSERT(e.val == *(int *)x->key) << e.val << " should equal " << *(int *)x->key;
+    if (noDuplicates)
+      ASSERT(e.info == x->info) << e.info << " should equal " << x->info;
+    idx = containerNext (idx);
+    InorderTreeVerify(tree,x->right);
+  }
+}
+
+void RBTreeVerify(rb_red_blk_tree* tree) {
+  idx = containerStart();
+  InorderTreeVerify(tree,tree->root->left);
+  ASSERT(idx == -1) << "idx should be -1!";
+}
+
 TEST(RBTree, TinySymex) {
   rb_red_blk_node* node;
   
@@ -103,4 +124,27 @@ TEST(RBTree, TinySymex) {
     ASSERT(!containerFind(key)) << "Expected not to find " << key;
   }      
 
+  key = DeepState_Int();
+  ip = (int*)malloc(sizeof(int));
+  *ip = key;
+  vp = voidP();
+  LOG(INFO) << "INSERT:" << *ip << " " << vp;  
+  RBTreeInsert(tree, ip, vp);
+  containerInsert(*ip, vp);  
+
+  key = DeepState_Int();
+  LOG(INFO) << "DELETE:" << key;  
+  if ((node = RBExactQuery(tree, &key))) {
+    ASSERT(containerFind(key)) << "Expected to find " << key;
+    RBDelete(tree, node);
+    containerDelete(key);
+  } else {
+    ASSERT(!containerFind(key)) << "Expected not to find " << key;
+  }      
+
+  LOG(INFO) << "checkRep...";
+  checkRep(tree); 
+  LOG(INFO) << "RBTreeVerify...";   
+  RBTreeVerify(tree);
+  
 }
